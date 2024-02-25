18b1bac2e643d8d3c77bc91c0a649d40adce5d07
MVMAM <ediconpts9@gmail.com>
2024-02-25 19:49:01 +0500

    Scroler to top

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ function hideSidebar() {
     sidebar.style.display = 'none';
 }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.bottom = "40px";
+    } else {
+        scrollToTopBtn.style.bottom = "700px";
+    }
+}
+
+document.getElementById("scrollToTopBtn").onclick = function() {
+    scrollToTop();
+};
+
+function scrollToTop() {
+    var position = document.documentElement.scrollTop || document.body.scrollTop;
+    if (position) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, position - position / 8);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelectorAll('.slide');
     const indicators = document.querySelectorAll('.indicator');
@@ -201,7 +224,7 @@ document.getElementById('submit-btn').addEventListener('click', function() {
         phone: document.getElementById('phone').value
     };
 
-    emailjs.send('SERVICE_ID', 'TEMPLATE_ID', formData)
+    emailjs.send('service_3g6gffm', 'template_r6oyzik', formData)
         .then(function(response) {
             console.log('Письмо успешно отправлено!', response.status, response.text);
         }, function(error) {
